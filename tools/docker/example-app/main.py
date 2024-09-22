364d5f946ea7ec15824195617c982f479701a9ee
Thelin90 <simon.thelin90@gmail.com>
2024-09-22 13:36:08 +0100

    Update setup

--- tools/docker/example-app/main.py
+++ tools/docker/example-app/main.py
@@ -9,9 +9,10 @@ def generate_random_number():
 
 if __name__ == '__main__':
     start_http_server(8000)
+    print("Start Prometheus Server 8000")
     
     while True:
         random_number = generate_random_number()
-        print('Random integer is: ', random_number)
+        print(f"Random integer is: {random_number}")
         random_number_metric.set(random_number)
         time.sleep(15)
