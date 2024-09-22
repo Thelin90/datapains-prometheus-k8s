7be22ed921115fbe78a6c266fcbe47a20415ff50
Thelin90 <simon.thelin90@gmail.com>
2024-09-22 13:19:39 +0100

    Update example app

---
+++ tools/docker/example-app/main.py
@@ -0,0 +1,17 @@
+import random
+import time
+from prometheus_client import start_http_server, Gauge
+
+random_number_metric = Gauge('random_integer', 'Random number generated every 15 sec')
+
+def generate_random_number():
+    return random.randint(1, 100000000)
+
+if __name__ == '__main__':
+    start_http_server(8000)
+    
+    while True:
+        random_number = generate_random_number()
+        print('Random integer is: ', random_number)
+        random_number_metric.set(random_number)
+        time.sleep(15)
